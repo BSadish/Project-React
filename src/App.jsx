aac5336958cbda74a93450f2f8a25a7afb9067d5
sadish panta <sadishpanta404@gmail.com>
2025-04-19 23:03:48 +0545

    little modification on the ui and made a proper crud

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,39 @@ import Nav from "./components/Nav";
 import { FiSearch } from "react-icons/fi";
 import { CiCirclePlus } from "react-icons/ci";
 
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+
+  onSnapshot,
+ 
+ 
+
+} from "firebase/firestore";
 import { db } from "./config/firebase";
 
 import Contact from "./components/Contact";
+import useDiscious from "./hooks/useDiscious";
+import AddandUpdate from "./components/AddandUpdate";
 
 const App = () => {
   const [contacts, setContact] = useState([]);
+  const { onClose, onOpen, isOpen } = useDiscious();
 
   useEffect(() => {
     const getcontract = async () => {
       try {
         const contactsRef = collection(db, "contact");
-        const contactsSnapshot = await getDocs(contactsRef);
-        const contactLists = contactsSnapshot.docs.map((doc) => {
-          return {
-            id: doc.id,
-            ...doc.data(),
-          };
+
+        onSnapshot(contactsRef, (snapshot) => {
+          const contactLists = snapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            };
+          });
+          setContact(contactLists);
+          return contactLists;
         });
-        setContact(contactLists);
       } catch (error) {
         console.log(error);
       }
@@ -30,25 +43,32 @@ const App = () => {
     getcontract();
   }, []);
   return (
-    <div className="mx-auto max-w-[370px] px-4">
-      <Nav />
-      <div className="flex gap-2">
-        <div className="flex relative items-center flex-grow">
-          <FiSearch className="text-white text-3xl absolute ml-1" />
-          <input
-            type="text"
-            className="bg-transparent border-2 border-white rounded-[4px] h-10 flex-grow text-white pl-9"
+    <>
+      <div className="mx-auto max-w-[370px] px-4">
+        <Nav />
+        <div className="flex gap-2">
+          <div className="flex relative items-center flex-grow">
+            <FiSearch className="text-white text-3xl absolute ml-1" />
+            <input
+              type="text"
+              className="bg-transparent border-2 border-white rounded-[4px] h-10 flex-grow text-white pl-9"
+            />
+          </div>
+
+          <CiCirclePlus
+            onClick={onOpen}
+            className="text-white text-5xl cursor-pointer"
           />
         </div>
-
-        <CiCirclePlus className="text-white text-5xl cursor-pointer" />
+        <div className="mt-4 flex flex-col gap-3">
+          {contacts.map((contact) => (
+            <Contact key={contact.id} contact={contact} />
+          ))}
+        </div>
       </div>
-      <div className="mt-4">
-  {contacts.map((contact)=>(
-    <Contact key={contact.id} contact={contact}/>
-  ))}
-</div>
-    </div>
+
+      <AddandUpdate onClose={onClose} isOpen={isOpen} />
+    </>
   );
 };
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,10 +2,26 @@ import React from 'react'
 import { HiOutlineUserCircle } from "react-icons/hi";
 import { RiEditCircleLine } from "react-icons/ri";
 import { IoMdTrash } from "react-icons/io";
+import {db} from '../config/firebase'
+import { deleteDoc, doc } from 'firebase/firestore';
+import AddandUpdate from './AddandUpdate';
+import useDiscious from "../hooks/useDiscious"
+
 const Contact = ({contact}) => {
+
+    const {onClose, onOpen, isOpen}=useDiscious();
+
+    const deletecontact=async(id)=>{
+        try {
+            
+            await deleteDoc(doc(db,"contact",id))
+        } catch (error) {
+            console.log(error)
+        }
+    }
   return (
-   
-      <div key={contact.id} className="bg-yellow-200 flex justify-between p-2 rounded-lg mb-1">
+   <>
+      <div key={contact.id} className="bg-yellow-200 flex justify-between p-2 rounded-lg ">
         <div className="flex gap-1 ">
           <HiOutlineUserCircle className="text-orange-500 text-4xl" />
           <div className="">
@@ -15,11 +31,12 @@ const Contact = ({contact}) => {
         </div>
 
         <div className="flex items-center text-3xl">
-          <RiEditCircleLine />
-          <IoMdTrash className="text-orange-400" />
+          <RiEditCircleLine onClick={onOpen} className='cursor-pointer'/>
+          <IoMdTrash onClick={()=>deletecontact(contact.id)} className="text-orange-400 cursor-pointer" />
         </div>
       </div>
- 
+      <AddandUpdate contact={contact} isUpdate isOpen={isOpen} onClose={onClose}/>
+      </>
   )
 }
 
